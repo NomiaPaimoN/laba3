ae15c95aa080a35bf444267c789fff44a6c8904c
gogop <gogop@TEMA>
2022-09-23 22:58:53 +0300

    Добавьте файлы проекта.

---
+++ laba3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include <math.h>
+using namespace std;
+int main()
+{
+	double a, b, h, x, y, s, p;
+	int n, i;
+	cout << "Vvedite a,b,n,h" << endl;
+	cin >> a >> b >> n >> h;
+	x = a;
+	do
+	{
+		p = s = 1;
+		for (i = 1; i <= n; i++)
+		{
+			p *= 2*x / i;
+			s += p;
+		}
+		y = exp(2*x);
+		cout << setw(15) << x << setw(15) << y << setw(15) << s << endl;
+		x += h;
+	} while (x <= b + h / 2);
+	cout << endl;
+	return 0;
+}
